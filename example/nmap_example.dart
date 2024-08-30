d3d799dc3813e1a0dec3a1acac576ab3ea4a0bea
gruvw <gruvw.dev@gmail.com>
2024-08-30 08:24:54 +0200

    docs: write example dart

--- example/nmap_example.dart
+++ example/nmap_example.dart
@@ -1,6 +1,9 @@
-import 'package:nmap/nmap.dart';
+import "package:nmap/nmap.dart";
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final int? a = null;
+  final int b = 1;
+
+  print(a.nmap((n) => n + 1)); // null
+  print(b.nmap((n) => n + 1)); // 2
 }
--- roadmap.md
+++ roadmap.md
@@ -5,5 +5,7 @@
 - [X] Code the actual extension
 - [X] Write tests
 - [X] Code documentation
+- [X] Write examples
 - [ ] Write README
+- [ ] CI for push validation
 - [ ] CI for release on pub.dev
